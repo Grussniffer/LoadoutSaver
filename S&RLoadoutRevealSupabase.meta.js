14f6c464bf055b5352271b627d99d1a1262a97f5
Grussniffer <126242825+Grussniffer@users.noreply.github.com>
2026-03-14 17:14:03 +0100

    Add metadata for S&R Loadout Aggregator script

---
+++ S&RLoadoutRevealSupabase.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         S&R Loadout Aggregator
+// @namespace    sr.loadout.logger
+// @version      1.3.0
+// @license      All Rights Reserved
+// @description  Captures Torn attack data and renders saved loadouts.
+// @author       You
+// @match        https://www.torn.com/loader.php?sid=attack&user2ID=*
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+// @connect      supabase.grusmedia.no
+// @run-at       document-start
+// @downloadURL  https://raw.githubusercontent.com/YOURUSER/YOURREPO/main/S-R-Loadout-Aggregator.user.js
+// @updateURL    https://raw.githubusercontent.com/YOURUSER/YOURREPO/main/S-R-Loadout-Aggregator.meta.js
+// ==/UserScript==
