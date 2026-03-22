499856e8e0625ad42bf93613faa3bd2c3347e391
Grussniffer <126242825+Grussniffer@users.noreply.github.com>
2026-03-23 00:10:09 +0100

    Bump version from 3.1.0 to 3.2.0

--- LoadoutRevealSupabase.meta.js
+++ LoadoutRevealSupabase.meta.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Loadout Loader
 // @namespace    loadout.loader
-// @version      3.1.0
+// @version      3.2.0
 // @description  Captures Torn attack data and renders saved loadouts through backend API.
 // @author       Sneip
 // @match        https://www.torn.com/loader.php?sid=attack&user2ID=*
 // @grant        GM_xmlhttpRequest
 // @grant        unsafeWindow
+// @connect      askelads.grusmedia.no
 // @connect      grusmedia.no
 // @run-at       document-start
 // @downloadURL  https://raw.githubusercontent.com/Grussniffer/LoadoutSaver/main/LoadoutRevealSupabase.user.js
